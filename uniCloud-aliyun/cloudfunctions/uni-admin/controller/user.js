3287a8d0579286e2fb9cba8820b33fc749468b79
xlzy <1017866168@qq.com>
2021-05-28 12:22:56 +0800

    feat: 增加uni_modules和uniCloud

---
+++ uniCloud-aliyun/cloudfunctions/uni-admin/controller/user.js
@@ -0,0 +1,80 @@
+const {
+	Controller
+} = require('uni-cloud-router')
+module.exports = class UserController extends Controller {
+	async login() {
+		const {
+			username,
+			password,
+			captchaText,
+			captchaOptions
+		} = this.ctx.data
+		return this.service.user.login({
+			username,
+			password,
+			captchaText,
+			captchaOptions
+		})
+	}
+
+	async registerBySMS() {
+		const {
+      mobile,
+      code,
+      ...params
+		} = this.ctx.data
+    if (!code) {
+      return {
+        code: 500,
+        msg: '请填写验证码'
+      }
+    }
+    if (!/^1\d{10}$/.test(mobile)) {
+      return {
+        code: 500,
+        msg: '手机号码填写错误'
+      }
+    }
+    // res = await uniIDIns.loginBySms(params)
+		
+		return this.ctx.uniID.register({
+      mobile,
+			role: ["user"],
+      ...params
+		})
+	}
+	
+	async register() {
+		const {
+			username,
+			password,
+      ...params
+		} = this.ctx.data
+		const admin = await this.service.user.hasAdmin()
+		if (admin) {
+			return {
+				code: 10001,
+				message: '超级管理员已存在，请登录...'
+			}
+		}
+		
+		return this.ctx.uniID.register({
+			username,
+			password,
+			role: ["admin"],
+      ...params
+		})
+	}
+
+	async logout() {
+		return this.service.user.logout(this.ctx.event.uniIdToken)
+	}
+
+	async createCaptcha() {
+		return await this.service.user.createCaptcha(this.ctx.data)
+	}
+
+	async getNeedCaptcha() {
+		return await this.service.user.getNeedCaptcha(this.ctx.data)
+	}
+}
